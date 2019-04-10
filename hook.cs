6333ed6b3662cefafa9e185f84beb507e253ab34
paul0727 <31685277+paul0727@users.noreply.github.com>
2019-04-10 17:05:28 +0800

    Add files via upload

---
+++ hook.cs
@@ -0,0 +1,36 @@
+public partial class hook : Window
+    {
+        KeyboardHook k_hook;
+        public hook()
+        {
+            InitializeComponent();
+            k_hook = new KeyboardHook();
+            //k_hook.KeyDownEvent += new System.Windows.Forms.KeyEventHandler(hook_KeyDown);//钩住键按下 
+            k_hook.KeyPressEvent += K_hook_KeyPressEvent;
+            k_hook.Start();//安装键盘钩子
+        }
+
+        private void K_hook_KeyPressEvent(object sender, KeyPressEventArgs e)
+        {
+            //tb1.Text += e.KeyChar;
+            int i = (int)e.KeyChar;
+            System.Windows.Forms.MessageBox.Show(i.ToString());
+        }
+
+        private void hook_KeyDown(object sender, KeyEventArgs e)
+        {
+            tb1.Text += (char)e.KeyData;
+
+
+            //判断按下的键（Alt + A） 
+            //if (e.KeyValue == (int)Keys.A && (int)System.Windows.Forms.Control.ModifierKeys == (int)Keys.Alt)
+            //{
+            //    System.Windows.Forms.MessageBox.Show("ddd");
+            //}
+        }
+
+        private void Window_Unloaded(object sender, RoutedEventArgs e)
+        {
+            k_hook.Stop();
+        }
+    }
